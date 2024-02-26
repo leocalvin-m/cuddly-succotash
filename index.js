a12919075b4570462ae6de01cb61c7e0f6f8716c
leocalvin <leocalvin.m@gmail.com>
2024-02-26 18:49:10 +0530

    added websockets

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const http = require("http");
+const server = require("http").createServer();
+const express = require("express");
+const WSServer = require("ws").Server;
+const app = express();
 
-http.createServer((req, res)=>{
-	res.write("HiHello again Nice Try");
-	res.end();
-}).listen(3000);
+app.get("/",(req, res) => {
+	res.sendFile('index.html',{root:__dirname});
+})
+server.on("request",app);
 
-console.log("Hi Server running on port 3000.....");
+server.listen(3000,() => console.log("Listeing on port 3000"))
+
+const wss = new WSServer({server:server});
+
+wss.on('connection',(ws)=>{
+
+	const numofClinets = wss.clients.size;
+	console.log("clients Connected: "+numofClinets);
+	wss.broadcast("current visitors: "+ numofClinets);
+	if(wss.readyState === ws.OPEN){
+		wss.send("welcome to MOBSTORE");
+	}
+
+	wss.on('close',()=>{
+		console.log("A Client has Disconnected");
+	})
+})
+
+wss.broadcast = (data) => {
+	wss.clients.forEach((client) =>{
+		client.send(data);
+	})
+}
