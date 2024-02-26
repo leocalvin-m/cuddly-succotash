4ed802057b011138587d54c428a2f454419cac17
leocalvin <leocalvin.m@gmail.com>
2024-02-26 19:53:03 +0530

    added DB Connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require("http").createServer();
+const sqlite = require("sqlite3");
 const express = require("express");
+
 const WSServer = require("ws").Server;
 const app = express();
 
@@ -7,10 +9,17 @@ app.get("/",(req, res) => {
 	res.sendFile('index.html',{root:__dirname});
 })
 server.on("request",app);
-
 server.listen(3000,() => console.log("Listeing on port 3000"))
 
 const wss = new WSServer({server:server});
+
+process.on("SIGINT", () => {
+	wss.clients.forEach(client => client.close());
+	server.close(() => {
+		shutdownDB();
+	})
+})
+
 
 wss.on('connection',(ws)=>{
 
@@ -21,6 +30,9 @@ wss.on('connection',(ws)=>{
 		wss.send("welcome to MOBSTORE");
 	}
 
+	db.run(`INSERT INTO visitors (count, time)
+	VALUES (${numofClinets}, datetime('now'))
+	`);
 	wss.on('close',()=>{
 		console.log("A Client has Disconnected");
 	})
@@ -30,4 +42,27 @@ wss.broadcast = (data) => {
 	wss.clients.forEach((client) =>{
 		client.send(data);
 	})
+}
+
+const db = new sqlite.Database(":memory");
+
+db.serialize(() => {
+	db.run(
+		`CREATE TABLE visitors(
+			count INTEGER,
+			time TEXT
+		)`
+	)
+})
+
+const getCounts = () => {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log(row);
+	})
+}
+
+const shutdownDB = () => {
+	getCounts();
+	console.log("shutting down...");
+	db.close();	
 }
